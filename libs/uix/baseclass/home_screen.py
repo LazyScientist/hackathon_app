f3570406e5ca850031798eb27c29f49f4c6f3fb5
pankaj_jangir <xpscientist@gmail.com>
2022-03-23 12:47:38 +0530

    updates

--- libs/uix/baseclass/home_screen.py
+++ libs/uix/baseclass/home_screen.py
@@ -1,4 +1,6 @@
 from kivymd.uix.screen import MDScreen
+from libs.uix.components.kivy_garden.mapview import MapView
+
 
 
 class HomeScreen(MDScreen):
@@ -6,6 +8,10 @@ class HomeScreen(MDScreen):
     
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.mapview = MapView(zoom=11, lat=50.6394, lon=3.057)
+
+
+        self.ids.map_card.add_widget(self.mapview)
         
     def btn_press_test(self):
         print("its working")
